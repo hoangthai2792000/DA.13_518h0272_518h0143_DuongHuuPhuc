1c1a69a6e1adec52cabec81a635949e6012c0f15
THAI <hoangthai2792000@gmail.com>
2022-10-25 11:31:39 +0700

    complete register function

--- server/models/User.js
+++ server/models/User.js
@@ -46,11 +46,26 @@ const UserSchema = new mongoose.Schema({
       message: 'Please provide valid phone number',
     },
   },
+  verificationToken: {
+    type: String,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verified: {
+    type: Date,
+  },
+  passwordToken: {
+    type: String,
+  },
+  passwordTokenExpired: {
+    type: Date,
+  },
 })
 
 UserSchema.pre('save', async function () {
   if (!this.isModified('password')) return
-
   const salt = await bcrypt.genSalt(10)
   this.password = await bcrypt.hash(this.password, salt)
 })
---
+++ server/utils/nodeMailerConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+    user: 'juliet.vonrueden@ethereal.email',
+    pass: 'YXc12WuUJc7Dh6Frtx',
+  },
+}
