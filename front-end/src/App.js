41df7d70452ba5b8f2c9c470e515fae8d1f187bc
chinhcvng1518 <chinhcvng1518@gmail.com>
2022-11-11 14:03:27 +0700

    update dependencies

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,29 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Home from './pages/Home';
-import Register from './pages/Register';
-import Login from './pages/Login';
-
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
+import Home from './pages/HomePage';
+import About from './pages/AboutPage';
+import Cart from './pages/CartPage';
+import Products from './pages/ProductPage';
+import Error from './pages/ErrorPage';
+import SingleProduct from './pages/SingleProductPage';
+import PrivateRoute from './pages/PrivateRoute';
+import Checkout from './pages/CheckoutPage';
 function App() {
   return (
-    <>
       <Router>
-        <Switch>
-          <Route exact path='/'>
-            <Home/>
-          </Route>
-          <Route exact path='/login'>
-            <Login/>
-          </Route>
-          <Route exact path='/register'>
-            <Register/>
-          </Route>
-        </Switch>
+        <Navbar/>
+        <Sidebar/>
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/about' element={<About/>}/>
+          <Route exact path='/cart' element={<Cart/>}/>
+          <Route exact path='/products' element={<Products/>}/>
+          <Route path='*' element={<Error/>}/>
+        </Routes>
+        <Footer/>
       </Router>
-    </>
   );
 }
 
