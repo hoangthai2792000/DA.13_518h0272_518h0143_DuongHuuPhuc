aa3f7481705a4b81fe4713a955b1124481742d88
chinhcvng1518 <chinhcvng1518@gmail.com>
2022-11-11 14:01:33 +0700

    update dependencies

---
+++ front-end/src/components/Hero.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import logo from '../assets/logo.jpg'
+
+const Hero = () => {
+  return (
+    <Wrapper className='section-center'>
+      <article className='content'>
+        <h1>
+
+          Design your <br/>
+          comfort zone
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iure
+          quasi odit tenetur unde officiis repudiandae quod deserunt quia eum?
+        </p>
+      <Link to='/products' className='btn hero-btn'>
+        Shop now
+      </Link>
+      </article>
+      <article className='img-container'>
+        <img src={logo} alt='nice sneaker' className='main-img'/>
+        <img src={logo} alt='nice sneaker1' className='main-img'/>
+      </article>
+    </Wrapper>
+  )
+}
+const Wrapper = styled.div`
+`
+
+export default Hero
